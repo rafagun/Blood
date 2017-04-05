7f2aa3f93ad717c6ff2d68d2aa5e112b83ee0905
claudia <clauus4.csb@gmail.com>
2017-04-05 10:41:59 +0200

    delete

--- src/Blood/db/jdbc/DB_Manager.java
+++ src/Blood/db/jdbc/DB_Manager.java
@@ -1,5 +1,6 @@
 package Blood.db.jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,203 +13,221 @@ import Blood.db.pojos.Hospital;
 
 
 public class DB_Manager {
-	Connection c;
+Connection c;
 
 
-	public void SQLConnect() {
-			try {
-				// Open database connection
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:./db/blood.db");
-				c.createStatement().execute("PRAGMA foreign_keys=ON");
-				System.out.println("Database connection opened.");
-				
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-	
+public void SQLConnect() {
+try {
+// Open database connection
+Class.forName("org.sqlite.JDBC");
+c = DriverManager.getConnection("jdbc:sqlite:./db/blood.db");
+c.createStatement().execute("PRAGMA foreign_keys=ON");
+System.out.println("Database connection opened.");
+
 }
-	public void SQLDisconnect()
-	{
-		// Close database connection
-		try {
-			c.close();
-			System.out.println("Database connection closed.");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void SQLCreate() throws SQLException {
-		try {
-			
-			// Create tables: begin
-			Statement stmt1 = c.createStatement();
-			String sql1 = "CREATE TABLE Hospital"
-					 + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					 + " name     TEXT     NOT NULL, "
-				    + " location  TEXT,"
-					+ " range  INTEGER)";
-			stmt1.executeUpdate(sql1);
-			stmt1.close();
-			/** Statement stmt2 = c.createStatement();
-			String sql2 = "CREATE TABLE Nurses "
-					  + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					  + " name     TEXT     NOT NULL, "
-					  + " photo    BLOB , "
-					  + " hospital_id  INTEGER  REFERENCES hospital(id) ON DELETE CASCADE ) ";
-				
-			stmt2.executeUpdate(sql2);
-			stmt2.close();
-			Statement stmt3 = c.createStatement();
-			String sql3 = "CREATE TABLE Patient "
-					  + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					  + " name     TEXT     NOT NULL, "
-					  + " age      INTEGER, "
-					  + " blood	TEXT     NOT NULL,"
-					  + "smoker     BOOLEAN "
-					  + "gender     STRING NOT NULL)";
-			stmt3.executeUpdate(sql3);
-			stmt3.close();
-			Statement stmt4 = c.createStatement();
-			String sql4 = "CREATE TABLE authors "
-					  + "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					  + " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					  + " PRIMARY KEY (report_id,employee_id))";
-			stmt4.executeUpdate(sql4);
-			stmt4.close(); **/
-			System.out.println("Tables created.");
-			// Create table: end
-			
-			// - Set initial values for the Primary Keys
-			// - Don't try to understand this until JPA is explained
-			// This is usually not needed, since the initial values
-			// are set when the first row is inserted, but since we
-			// are using JPA and JDBC in the same project, and JPA
-			// needs an initial value, we do this.
-			/**Statement stmtSeq = c.createStatement();
-			String sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('departments', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('employees', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('reports', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			stmtSeq.close();**/
-			
+catch (Exception e) {
+e.printStackTrace();
+}
+
+}
+public void SQLDisconnect()
+{
+// Close database connection
+try {
+c.close();
+System.out.println("Database connection closed.");
+} catch (SQLException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+}
+public void SQLCreate() throws SQLException {
+try {
+
+// Create tables: begin
+Statement stmt1 = c.createStatement();
+String sql1 = "CREATE TABLE Hospital "
++ "(id INTEGER PRIMARY KEY AUTO INCREMENT , "
++ "name TEXT NOT NULL, "
++ "location  TEXT, "
++ "range  INTEGER)";
+stmt1.executeUpdate(sql1);
+stmt1.close();
+/** Statement stmt2 = c.createStatement();
+String sql2 = "CREATE TABLE Nurses "
+  + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+  + " name     TEXT     NOT NULL, "
+  + " photo    BLOB , "
+  + " hospital_id  INTEGER  REFERENCES hospital(id) ON DELETE CASCADE ) ";
+
+stmt2.executeUpdate(sql2);
+stmt2.close();
+Statement stmt3 = c.createStatement();
+String sql3 = "CREATE TABLE Patient "
+  + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+  + " name     TEXT     NOT NULL, "
+  + " age      INTEGER, "
+  + " blood	TEXT     NOT NULL,"
+  + "smoker     BOOLEAN "
+  + "gender     STRING NOT NULL)";
+stmt3.executeUpdate(sql3);
+stmt3.close();
+Statement stmt4 = c.createStatement();
+String sql4 = "CREATE TABLE authors "
+  + "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+  + " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+  + " PRIMARY KEY (report_id,employee_id))";
+stmt4.executeUpdate(sql4);
+stmt4.close(); **/
+System.out.println("Tables created.");
+// Create table: end
+
+// - Set initial values for the Primary Keys
+// - Don't try to understand this until JPA is explained
+// This is usually not needed, since the initial values
+// are set when the first row is inserted, but since we
+// are using JPA and JDBC in the same project, and JPA
+// needs an initial value, we do this.
+/**Statement stmtSeq = c.createStatement();
+String sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('departments', 1)";
+stmtSeq.executeUpdate(sqlSeq);
+sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('employees', 1)";
+stmtSeq.executeUpdate(sqlSeq);
+sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('reports', 1)";
+stmtSeq.executeUpdate(sqlSeq);
+stmtSeq.close();**/
+
+
+
+}
+catch (Exception e) {
+e.printStackTrace();
+}	
+}
+
+public void SQLSelect(){
+try {
+Statement stmt = c.createStatement();
+
+String sql = "SELECT * FROM Hospital";
+ResultSet rs = stmt.executeQuery(sql);
+while (rs.next()) {
+int id = rs.getInt("id");
+String name = rs.getString("name");
+String location = rs.getString("location");
+int range= rs.getInt("range");
+Hospital hospital = new Hospital (id,name,location,range);
+System.out.println(hospital);
+}
+rs.close();
+stmt.close();
+System.out.println("Search finished.");
+}
+catch(Exception ex){
+ex.printStackTrace();
+}
+}
+public void SQLDrop(){
+try {
+
+// Drop tables: begin
+Statement stmt1 = c.createStatement();
+String sql1 = "DROP TABLE hospitals";
+stmt1.executeUpdate(sql1);
+stmt1.close();
+/*Statement stmt2 = c.createStatement();
+String sql2 = "DROP TABLE nurses";
+stmt2.executeUpdate(sql2);
+stmt2.close();
+Statement stmt3 = c.createStatement();
+String sql3 = "DROP TABLE patients";
+stmt3.executeUpdate(sql3);
+stmt3.close();
+Statement stmt4 = c.createStatement();
+String sql4 = "DROP TABLE molecules";
+stmt4.executeUpdate(sql4);
+stmt4.close();
+Statement stmt5 = c.createStatement();
+String sql5 = "DROP TABLE cells";
+stmt5.executeUpdate(sql5);
+stmt5.close();
+Statement stmt6 = c.createStatement();
+String sql6 = "DROP TABLE symptoms";
+stmt6.executeUpdate(sql6);
+stmt6.close();
+Statement stmt7 = c.createStatement();
+String sql7 = "DROP TABLE illness";
+stmt7.executeUpdate(sql7);
+stmt7.close();
+System.out.println("Tables removed.");
+// Drop tables: end
+*/
+
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+public void SQLInsert(Hospital hospital){
+try {
+Statement stmt = c.createStatement();
+String sql = "INSERT INTO hospital (name, address, range) "
+//Se ponen comillas simples y comillas dobles porque las dobles dentro del parentesis se eliminan
+//con las de "VALUES" y por tanto quedan las comillas simples que son las necesarias en SQL.
++ "VALUES ('" + hospital.getName() + "', '" + hospital.getLocation()	+ "','"+ hospital.getRange() + "');";
+
+stmt.executeUpdate(sql);
+stmt.close();
+System.out.println("Patient information processed");
+System.out.println("Patient has been inserted.");
+
+
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+
+public void SQLSearch(String hospName) {
+try {
+String sql = "SELECT * FROM hospital WHERE name LIKE ?";
+PreparedStatement prep = c.prepareStatement(sql);
+prep.setString(1, hospName);
+ResultSet rs = prep.executeQuery();
+while (rs.next()) {
+int id = rs.getInt("id");
+String name = rs.getString("name");
+String location = rs.getString("location");
+int range = rs.getInt("range");
+
+Hospital hospital = new Hospital (id, name, location, range);
+System.out.println(hospital);
+
+}
+rs.close();
+prep.close();
+System.out.println("Search finished.");
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+
+public Hospital SQLDelete(String nameHospital) throws IOException, SQLException {
+	Hospital hospital = null;
+	String sql = "SELECT * FROM Hospital";
+	PreparedStatement prep = c.prepareStatement(sql);
+	prep.setString(1, nameHospital);
+	ResultSet rs = prep.executeQuery();
+	while (rs.next()) {
+		int id = rs.getInt("id");
+		String name = rs.getString("name");
+		String location = rs.getString("location");
+		int range = rs.getInt("range");
+		hospital = new Hospital(id, name, location,range);
 		
-
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}	
+	}
+	rs.close();
+	prep.close();
+	System.out.println("Delete is finished");
+	return hospital;
+    
 }
-
-	public void SQLSelect(){
-	try {
-		Statement stmt = c.createStatement();
-		
-		String sql = "SELECT * FROM Hospital";
-		ResultSet rs = stmt.executeQuery(sql);
-		while (rs.next()) {
-			int id = rs.getInt("id");
-			String name = rs.getString("name");
-			String location = rs.getString("location");
-			int range= rs.getInt("range");
-			Hospital hospital = new Hospital (id,name,location,range);
-			System.out.println(hospital);
-		}
-		rs.close();
-		stmt.close();
-		System.out.println("Search finished.");
 }
-	catch(Exception ex){
-		ex.printStackTrace();
-	}
-}
-	public void SQLDrop(){
-	try {
-		
-		// Drop tables: begin
-		Statement stmt1 = c.createStatement();
-		String sql1 = "DROP TABLE hospitals";
-		stmt1.executeUpdate(sql1);
-		stmt1.close();
-		/*Statement stmt2 = c.createStatement();
-		String sql2 = "DROP TABLE nurses";
-		stmt2.executeUpdate(sql2);
-		stmt2.close();
-		Statement stmt3 = c.createStatement();
-		String sql3 = "DROP TABLE patients";
-		stmt3.executeUpdate(sql3);
-		stmt3.close();
-		Statement stmt4 = c.createStatement();
-		String sql4 = "DROP TABLE molecules";
-		stmt4.executeUpdate(sql4);
-		stmt4.close();
-		Statement stmt5 = c.createStatement();
-		String sql5 = "DROP TABLE cells";
-		stmt5.executeUpdate(sql5);
-		stmt5.close();
-		Statement stmt6 = c.createStatement();
-		String sql6 = "DROP TABLE symptoms";
-		stmt6.executeUpdate(sql6);
-		stmt6.close();
-		Statement stmt7 = c.createStatement();
-		String sql7 = "DROP TABLE illness";
-		stmt7.executeUpdate(sql7);
-		stmt7.close();
-		System.out.println("Tables removed.");
-		// Drop tables: end
-	*/
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-}
-	public void SQLInsert(Hospital hospital){
-	try {
-		Statement stmt = c.createStatement();
-		String sql = "INSERT INTO hospital (name, address, range) "
-			//Se ponen comillas simples y comillas dobles porque las dobles dentro del parentesis se eliminan
-			//con las de "VALUES" y por tanto quedan las comillas simples que son las necesarias en SQL.
-				+ "VALUES ('" + hospital.getName() + "', '" + hospital.getLocation()	+ "','"+ hospital.getRange() + "');";
-		
-		stmt.executeUpdate(sql);
-		stmt.close();
-		System.out.println("Patient information processed");
-		System.out.println("Patient has been inserted.");
-
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-	}
-	
-	
-	public void SQLSearch(String hospName) {
-		try {
-			String sql = "SELECT * FROM hospital WHERE name LIKE ?";
-			PreparedStatement prep = c.prepareStatement(sql);
-			prep.setString(1, hospName);
-			ResultSet rs = prep.executeQuery();
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				String name = rs.getString("name");
-				String location = rs.getString("location");
-				int range = rs.getInt("range");
-				
-				Hospital hospital = new Hospital (id, name, location, range);
-				System.out.println(hospital);
-				
-		}
-			rs.close();
-			prep.close();
-			System.out.println("Search finished.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	}
-
--- src/Blood/db/ui/Interface.java
+++ src/Blood/db/ui/Interface.java
@@ -6,91 +6,99 @@ import java.sql.SQLException;
 import Blood.db.jdbc.*;
 import Blood.db.pojos.Hospital;
 public class Interface {
-	 public static void main(String[] args) throws SQLException 
-	    {
-	       
-	 InputStreamReader inputStreamReader = null;
-	 BufferedReader bufferedReader = null;
-	 DB_Manager f = null;
-	 try {
-	 inputStreamReader = new InputStreamReader(System.in);
-	 bufferedReader = new BufferedReader(inputStreamReader);
-	 //bucle infinito para mostrar nuestro menu hasta que pulsemos la opcion
-	 while (true){
-	     //mostramos por pantalla nuestro menu
-     System.out.println("Introduce 1 to connect ");
-	 System.out.println("Introduce 2 to create ");
-	 System.out.println("Introduce 3 to insert ");
-	 System.out.println("Introduce 4 to show");
-	 System.out.println("Introduce 5 to exit ");
+public static void main(String[] args) throws SQLException 
+{
 
-	 
+InputStreamReader inputStreamReader = null;
+BufferedReader bufferedReader = null;
+DB_Manager f = null;
+try {
+inputStreamReader = new InputStreamReader(System.in);
+bufferedReader = new BufferedReader(inputStreamReader);
+//bucle infinito para mostrar nuestro menu hasta que pulsemos la opcion
+while (true){
+//mostramos por pantalla nuestro menu
+System.out.println("Introduce 1 to connect ");
+System.out.println("Introduce 2 to create ");
+System.out.println("Introduce 3 to insert ");
+System.out.println("Introduce 4 to show");
+System.out.println("Introduce 5 to delete");
+System.out.println("Introduce 6 to exit ");
+
+
 int opcion = Integer.parseInt(bufferedReader.readLine());
 switch (opcion){
 
 case 1: 
-	f= new DB_Manager();
-	f.SQLConnect();
-	break;
+f= new DB_Manager();
+f.SQLConnect();
+break;
 
 
 case 2: 
-     f.SQLCreate();
-    break;
-    
+f.SQLCreate();
+break;
+
 case 3: 
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	System.out.println("Introduce the name of the hospital");
-	String nameHosp = reader.readLine();
-	System.out.println("Introduce the location of the hospital");
-	String locationHosp = reader.readLine();
-	System.out.println("Introduce the range of the hospital");
-	String rangeHospital = reader.readLine();
-	int rangeHosp = Integer.parseInt(rangeHospital);
-	Hospital hospitalInterface = new Hospital (nameHosp,locationHosp,rangeHosp);
-	f.SQLInsert(hospitalInterface);
-	break;
+BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Introduce the name of the hospital");
+String nameHosp = reader.readLine();
+System.out.println("Introduce the location of the hospital");
+String locationHosp = reader.readLine();
+System.out.println("Introduce the range of the hospital");
+String rangeHospital = reader.readLine();
+int rangeHosp = Integer.parseInt(rangeHospital);
+Hospital hospitalInterface = new Hospital (nameHosp,locationHosp,rangeHosp);
+f.SQLInsert(hospitalInterface);
+break;
 
 case 4:
-	BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
-	System.out.println("Introduce the name of the hospital that you want to search");
-	String nameHospKB = reader1.readLine();
-	f.SQLSearch(nameHospKB);
-	break;
-	
-case 5:	//salir del programa
-	f.SQLDisconnect();
-	System.exit(0);
+BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Introduce the name of the hospital that you want to search");
+String nameHospKB = reader1.readLine();
+f.SQLSearch(nameHospKB);
+break;
 
+case 5: 
+	Hospital hospitalDelete = null;
+	BufferedReader readerDelete = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Introduce the name of the hospital that you want to delete");
+	String nHospital = readerDelete.readLine();
+	hospitalDelete = f.SQLDelete(nHospital);
+	System.out.println("name:" +hospitalDelete.getName()+ "location:"+hospitalDelete.getLocation()+ "range:" +hospitalDelete.getRange());
+	break;
+
+case 6:	//salir del programa
+f.SQLDisconnect();
+System.exit(0);
 
 
 }
 }}
-	 catch (IOException ex)
-	 {
-	     //entrara en el catch y se lanzara una excepcion en el caso de que no se haya leido correctamente
-	     System.out.println("Se ha producido un error");
-	     ex.printStackTrace();
-	 } finally {
-	     //cerramos todas las variables que contengan algo 
-	     if (bufferedReader!=null){
-	         try{ 
-	             bufferedReader.close();
-	         } catch (IOException ex ){
-	             //volvera a lanzar una excepcion si no es posible 
-	             ex.printStackTrace();
-	         }
-	     }
-	      if (inputStreamReader!=null){
-	         try{ 
-	             inputStreamReader.close();
-	         } catch (IOException ex ){
-	             ex.printStackTrace();
-	         }
-	     }
-	     
-	 }
-	    }}
-	 
+catch (IOException ex)
+{
+//entrara en el catch y se lanzara una excepcion en el caso de que no se haya leido correctamente
+System.out.println("Se ha producido un error");
+ex.printStackTrace();
+} finally {
+//cerramos todas las variables que contengan algo 
+if (bufferedReader!=null){
+try{ 
+bufferedReader.close();
+} catch (IOException ex ){
+//volvera a lanzar una excepcion si no es posible 
+ex.printStackTrace();
+}
+}
+if (inputStreamReader!=null){
+try{ 
+inputStreamReader.close();
+} catch (IOException ex ){
+ex.printStackTrace();
+}
+}
+
+}
+}}
 
 	   
